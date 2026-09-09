1cab244a9247419bf56531d94a3df776f10eeb29
Dalisson Silva <dalissonmuniz@gmail.com>
2026-09-08 22:54:44 -0300

    Revert "Fix: Revisão de novo processo de criação das tabelas."
    This reverts commit 22d2242aa88587af252f7b231e819c86bf579c62.

--- dags/dag_btc_bronze_pipeline.py
+++ dags/dag_btc_bronze_pipeline.py
@@ -5,7 +5,6 @@ from airflow.operators.python import PythonOperator
 
 sys.path.append("/opt/airflow/scripts_root/current/scripts")
 
-from databricks_bootstrap_schema import main as bootstrap_schema
 from incremental_binance import main as extrair_incremental
 from databricks_upload import main as upload_parquet
 from databricks_merge_bronze import main as merge_bronze
@@ -24,11 +23,6 @@ with DAG(
     tags=["bitcoin", "bronze", "databricks"],
 ) as dag:
 
-    task_bootstrap = PythonOperator(
-        task_id="bootstrap_schema_bronze",
-        python_callable=bootstrap_schema,
-    )
-
     task_extrair = PythonOperator(
         task_id="extrair_incremental_binance",
         python_callable=extrair_incremental,
@@ -44,4 +38,4 @@ with DAG(
         python_callable=merge_bronze,
     )
 
-    task_bootstrap >> task_extrair >> task_upload >> task_merge
+    task_extrair >> task_upload >> task_merge
